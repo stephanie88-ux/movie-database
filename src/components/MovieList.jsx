8c3ad4ce8b9587a094ed3ac73d06b32600b674c2
stephaniechepkosgei <stephieem88@gmail.com>
2026-02-18 21:47:58 +0300

    link movie list to an API

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import { fetchPopularMovies } from "../services/api";
+import MovieCard from './MovieCard';
+
+function MovieList() {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null); 
+
+    useEffect(() => {
+        const getMovies = async () => {
+        console.log("testing")
+        console.log("PRINTING DATA")
+        setLoading(true);
+        try {
+            const data = await fetchPopularMovies();
+            console.log(data);
+            setData(data);
+        } catch (err) {
+            setError(err);
+        } finally {
+            setLoading(false);
+        }
+        };
+        getMovies();
+    }, []);
+
+
+    if (loading) {
+        return <p>Loading data...</p>;
+    }
+
+    if (error) {
+        return <p>Error: {error}</p>;
+    }
+    return (
+        <div className="columns-4 gap-4">
+            {/* <p>This is the movie list component</p> */}
+        {data.results.map((item) => (
+            <MovieCard movie={item}/>
+        ))}
+            {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+        </div> 
+    );
+}  
+
+export default MovieList    
