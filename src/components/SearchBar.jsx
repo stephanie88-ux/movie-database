825392c5080f7373e5afb531545ff7b8df28bbc1
stephaniechepkosgei <stephieem88@gmail.com>
2026-02-19 16:10:03 +0300

    searchBar component intergration with API

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,2 +1,24 @@
 import React from 'react';
 import { useState } from 'react';
+
+
+const searchBar = ({ onSeacrh }) => {
+
+const [ query, setQuery ] = useState('');
+
+const handleSubmit = (e) => {
+e.prventDefault (e);
+if (query.trim()) {
+onSeacrh (query); 
+ 
+}}};
+
+retun (
+    <div className=""
+
+     >
+
+     </div>
+)
+    
+
