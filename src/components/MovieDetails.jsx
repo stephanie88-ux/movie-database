8b972b6fceebf68a20c0b622f0ce1ff677c147ab
stephaniechepkosgei <stephieem88@gmail.com>
2026-02-26 14:44:30 +0300

    link moviedetails to API

---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from 'react';
+import { useSearchParams,useNavigate } from 'react-router-dom';
+import { getMovieDetails, getImageUrl } from '/..services/api'
+
+
+
+
