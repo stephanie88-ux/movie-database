702a72f216d387fcc061e5323560cbb212d62efa
stephaniechepkosgei <stephieem88@gmail.com>
2026-02-27 21:52:57 +0300

    async function fetching APi

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,6 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { useSearchParams,useNavigate } from 'react-router-dom';
+import { useSearchParams,useNavigate, useParams } from 'react-router-dom';
 import { getMovieDetails, getImageUrl } from '/..services/api'
+
+const MovieDetails= () => {
+const {id } = useParams ();
+const navigate = usenavigate ();
+
+const [movie, setMovie] = useState(null);
+const [load, setLoading] = useSate(true);
+const [error, setError] = useState(null);
+
+useEffect (() {
+    async function getMovie() {
+        const { data } = await axios.get('https://api.themoviedb.org/3');
+        setMovieDetails(data);
+
+    }
+
+}
+)
+ 
+
+
+     
 
 
 
