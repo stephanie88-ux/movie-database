13958b6bbb598c2fe2da20b7528c45138bbeb945
stephaniechepkosgei <stephieem88@gmail.com>
2026-03-01 01:04:53 +0300

    style loading, error, button using tailwind

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -7,6 +7,10 @@ const {id } = useParams ();
 const navigate = useNavigate ();
 
  
+const [movie, setMovie] = useState(null);        
+const [loading, setLoading] = useState(true);   
+const [error, setError] = useState(null);      
+ 
 useEffect(() => {
   const fetchMovieDetails = async () => {
     try {
@@ -24,6 +28,37 @@ useEffect(() => {
   fetchMovieDetails();
 }, [id]); 
 }
+
+ if (loading) {
+    return (
+      <div className="min-h-screen flex justify-center items-center bg-green-600">
+        <div className="text-center">
+        <p className="text-xl text-green-600">Loading movie details...</p>
+        </div>
+      </div>
+    );
+  }
+if (error || !movie) {
+    return(
+        <div className="mn-h-screen flex justufy-centre items centre bg-green-600">
+        <div className="text-centre">
+        <p className="text-xl text-green-600">error || movie not found</p>
+        </div>
+
+
+
+    <button
+            onClick={() => navigate('/')}
+            className="bg-emerald-900 hover:bg-blue-300 text-white 
+                     px-6 py-3 rounded-lg transition-colors"
+          >
+          </button>
+        </div>
+    );
+  }
+    
+ 
+
 
      
 
