3cd1fd59776054b06f5b7dd296fa4c11216077db
stephaniechepkosgei <stephieem88@gmail.com>
2026-03-02 23:18:44 +0300

    add search bar component resolve bugs

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,34 +2,49 @@ import React from 'react';
 import { useState } from 'react';
 
 
-const searchBar = ({ onSeacrh }) => {
+const SearchBar = ({ onSearch }) => {
 
-const [ query, setQuery ] = useState('');
+    const [ query, setQuery ] = useState('');
 
-const handleSubmit = (e) => {
-e.prventDefault (e);
-if (query.trim()) {
-onSeacrh (query); 
- 
-}}};
+    const handleSubmit = (e) => {
+        console.log("testing", query);
+        e.preventDefault();
+        if (query.trim()) {
+            onSearch(query); 
+        }
+    }
 
-retun (
-    <div className="w-full px-6 px-4 max-w-xl mx-auto">
-    <form onSubmit ={handleSubmit} className='relative'>
-    <input 
-    type="text"
-    value={query}
-    onChnge={(e) => setQuery(e.target.value)}
-    className="w-full px-6 px-4 max-w-xl mx-auto tx-lg rounded-full border-gray-200">
-
-    <button type="button" class="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Submit</button>
-
-    </input>
-    </form>
-    </div>
-
-);
-
-export default searchBar;
+    const handleChange = (e) => {
+        console.log(query);
+        setQuery(e.target.value);
+    };
     
 
+
+    return (
+        <div class="w-full max-w-sm min-w-[200px]">
+        <form class="relative" onSubmit={handleSubmit}>
+          <input
+            class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-3 pr-28 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+            placeholder="Search for a movie..." 
+            onChange={handleChange}
+          />
+          <button
+            class="absolute top-1 right-1 flex items-center rounded bg-slate-800 py-1 px-2.5 border border-transparent text-center text-sm text-white transition-all shadow-sm hover:shadow focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            type="submit"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 mr-2">
+             <path fill-rule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
+            </svg>
+       
+            Search
+          </button> 
+        </form>
+      </div>
+
+    );
+};
+
+
+
+export default SearchBar;
