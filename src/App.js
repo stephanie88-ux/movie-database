642443a18e8219257389bdf210af312a74586d38
stephaniechepkosgei <stephieem88@gmail.com>
2026-03-02 23:31:41 +0300

    add routing and movieprovider to app j.s

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import logo from './film.png';
-import './App.css';
+import Logo from './film.png';
 import MovieList from './components/MovieList';
+import MovieDetails from './components/MovieDetails';
 import Nav from './components/Nav';
+import { MovieProvider } from './components/MovieContext';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="container mx-auto px-4">
-      <header>
-        <Nav/>
-      </header>
-      <MovieList/>
-    </div>
+    <MovieProvider>
+      <BrowserRouter>
+      <div className="container mx-auto px-4">
+        <header>
+          <Nav/>
+        </header>
+        <Routes>
+          <Route path="/" element={<MovieList />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+    </MovieProvider>
   );
 }
 
