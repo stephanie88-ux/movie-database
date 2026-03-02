041d3867ffaa75da494ffef6ab0c96c3c94dc384
stephaniechepkosgei <stephieem88@gmail.com>
2026-03-02 23:22:07 +0300

    fix reloading time using home button

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,24 @@
 import logo from '../film.png';
+import SearchBar from './SearchBar';
+import { useMovies } from './MovieContext';
 
 function Nav() {
+    const { fetchMovies } = useMovies();
+
+    const handleSearchSubmit = (query) => {
+        fetchMovies(query);
+    };
+
     return(
         <nav className='flex p-4 m-4 justify-evenly items-center'>
             <img className="w-12" src={logo} alt="logo" />
-            <a href="/HOME/">home</a> |
+            <a href="/">home</a> |
             <a href="/SEARCH/">search</a> |
             <a href="/NOTIFICATION/">notification</a> |
             <a href="/PROFILE/">profile</a>
+            <SearchBar onSearch={handleSearchSubmit} />
         </nav>
+
     );
 }
 
